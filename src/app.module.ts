ecfcfab1d9ec921c9bbcb721969a5bb6ab9e9e9c
Miguel Angel Martinez Rodriguez <miguel_angel-m@outlook.com>
2024-06-05 17:30:53 -0400

    chore: configure TypeORM with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Cambia esto según tu configuración
+      port: 5432, // Cambia esto según tu configuración
+      username: 'postgres', // Cambia esto según tu configuración
+      password: '123456', // Cambia esto según tu configuración
+      database: 'TestNestJs', // Cambia esto según tu configuración
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // No se recomienda en producción
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
