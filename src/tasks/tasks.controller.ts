c17e30e397c838a5162a296be451d4e2bc7ad3b4
Miguel Angel Martinez Rodriguez <miguel_angel-m@outlook.com>
2024-06-05 17:40:50 -0400

    feat(tasks): implement get all tasks feature

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/task.dto';
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Promise<Task[]> {
+    return this.tasksService.getAllTasks();
+  }
 
   @Post()
   createTask(@Body() newTask: CreateTaskDto) {
